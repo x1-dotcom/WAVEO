462a87ff2d69fc6fe7a58cfb7854f3100d0a3734
X1 Architect <carlosgago@ipcbcampus.pt>
2026-08-19 19:13:21 +0200

    Remove obsolete PowerShell assembly script

--- release/assemble.ps1
+++
@@ -1,4 +0,0 @@
-$parts = Get-ChildItem "X1_WAVEO_PANEL_v1.4_PUBLIC_OBFUSCATED_RC.zip.b64.part*" | Sort-Object Name
-$b64 = ($parts | ForEach-Object { Get-Content $_ -Raw }) -join ""
-[IO.File]::WriteAllBytes("X1_WAVEO_PANEL_v1.4_PUBLIC_OBFUSCATED_RC.zip",[Convert]::FromBase64String($b64))
-Get-FileHash "X1_WAVEO_PANEL_v1.4_PUBLIC_OBFUSCATED_RC.zip" -Algorithm SHA256
